5e06af815548dcb994540c147af183c3b03ad2b2
stefafrontani <stefafrontani@gmail.com>
2026-03-04 17:45:09 -0300

    update DashcamProduct interface

--- src/domain/entities/DashcamProduct.ts
+++ src/domain/entities/DashcamProduct.ts
@@ -1,23 +1,17 @@
 import type { CameraPosition } from '@/domain/value-objects/CameraPosition'
 import type { VideoQuality } from '@/domain/value-objects/VideoQuality'
-import type { RecordingTime } from '@/domain/value-objects/RecordingTime'
 
 export interface DashcamProduct {
   id: string
   name: string
-  brand: string
   description: string
-  price: number
   priceDisplay: string
-  rating: number
-  reviewCount: number
+  priceFinalDisplay: string
+  discount: string | null
+  priceAllCashDisplay: string
   specs: string[]
   features: string[]
   tags: string[]
   cameraPositions: CameraPosition[]
   maxQuality: VideoQuality
-  maxRecordingTime: RecordingTime
-  supportsParking: boolean
-  supportsBluetooth: boolean
-  supportsInstallation: boolean
 }
